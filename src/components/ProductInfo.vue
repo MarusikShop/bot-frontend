c27871b423193d0048d8d58fdcd199263227693e
Fedos16 <41968433+Fedos16@users.noreply.github.com>
2023-03-10 22:21:48 +0300

    delete link to basket from product in basket

--- src/components/ProductInfo.vue
+++ src/components/ProductInfo.vue
@@ -39,7 +39,6 @@
             <div class="product-info__actions">
                 <ProductToBasket :data="data" :modification="activeModification" />
             </div>
-            <RouterLink to="/basket">Перейти в корзину</RouterLink>
         </div>
     </div>
 </template>
